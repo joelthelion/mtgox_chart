0b12edd2b1b74180e0610c22d83659396da5714b
Joël Schaerer <joel.schaerer@gmail.com>
2011-06-14 12:06:50 +0200

    make the script suitable for distribution

--- plot_btc.py
+++ plot_btc.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python2
 # coding: utf-8
+"""
+This simple script parses the Mt. Gox trade data and displays it in a
+nice chart. It needs Matplotlib to work. By default it works on a local
+version of the code. Change the relevant line to enable live data.
+
+The code is licensed under the GNU Affero GPL, version 3
+(http://www.gnu.org/licenses/agpl.html), and copyright Joël Schaerer, 211
+"""
+
 from json import loads,load
 from urllib2 import urlopen
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 from datetime import datetime
 
+# Change here to use the live data
 #data=load(urlopen("http://mtgox.com/code/data/getTrades.php"))
 data=load(open("getTrades.php"))
 
