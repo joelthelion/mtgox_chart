f945a9ac82c59d4497a3a1f8d6a7ba04afda3f20
Joel Schaerer dedibox <joel.schaererl@laposte.net>
2011-06-20 16:15:37 +0200

    add support for tradehill

--- plot_btc.py
+++ plot_btc.py
@@ -15,37 +15,45 @@ from matplotlib.figure import Figure
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 from datetime import datetime
 
-# Change here to use the live data
-print "Downloading the data from Mt. Gox ..."
-try:
-    json = None
-    json = urlopen("https://mtgox.com/code/data/getTrades.php").read()
-    data = loads(json)
-except Exception as e:
-    if json: 
-        print json
-    raise
-#data=load(open("getTrades.php"))
+def make_graph(exchange_name, url):
+    print "Downloading the data from %s ..." % exchange_name
+    try:
+        json = None
+        json = urlopen(url).read()
+        data = loads(json)
+    except Exception as e:
+        if json: 
+            print json
+        print e
+        print "Connection problem, giving up for %s" % exchange_name
+        return
 
-print "Parsing the data..."
-date,price,amount = [],[],[]
-for d in data:
-    date.append(datetime.fromtimestamp(d["date"]))
-    price.append(d["price"])
-    amount.append(d["amount"])
+    print "Parsing the data..."
+    date,price,amount = [],[],[]
+    for d in data:
+        date.append(datetime.fromtimestamp(d["date"]))
+        price.append(float(d["price"]))
+        amount.append(float(d["amount"]))
 
-print "Plotting..."
-fig=Figure(figsize=(10,6))
-ax1=fig.add_subplot(111)
+    print "Plotting..."
+    fig=Figure(figsize=(10,6))
+    ax1=fig.add_subplot(111)
 
 # Tweak the alpha setting to change the transparency of each circle
-ax1.scatter(date,price,s=amount,alpha=0.1)
+    ax1.scatter(date,price,s=amount,alpha=0.1)
 
-fig.autofmt_xdate()
-ax1.set_xlabel("Time (UTC)")
-ax1.set_ylabel("Price ($/BTC)")
-ax1.set_title("Latest transactions at Mt. Gox")
-canvas=FigureCanvasAgg(fig)
-print "Rasterizing..."
-canvas.print_figure("mtgox.png",dpi=200)
-print "Done!"
+    fig.autofmt_xdate()
+    ax1.set_xlabel("Time (UTC)")
+    ax1.set_ylabel("Price ($/BTC)")
+    ax1.set_title("Latest transactions at %s" % exchange_name)
+    canvas=FigureCanvasAgg(fig)
+    print "Rasterizing..."
+    canvas.print_figure("%s.png"%exchange_name,dpi=200)
+    print "Done!"
+
+# Change here to use the live data
+#for exchange_name,url in [("local","getTrades.php")]:
+for exchange_name,url in [("mtgox","https://mtgox.com/code/data/getTrades.php"),
+        ("tradehill","https://api.tradehill.com/APIv1/USD/Trades")]:
+    make_graph(exchange_name,url)
+
